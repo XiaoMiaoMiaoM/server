4fcf6c3d7830a694ce41d3266c3c932e3d269f6e
xiaomiao <xiaomiao@example.com>
2018-09-20 08:07:38 +0800

    up

--- routes/index.js
+++ routes/index.js
@@ -70,4 +70,34 @@ router.post('/login',function (req,res) {
     }
   })
 })
+//更新用户路由
+router.post('/update',function (req,res) {
+  const userid = req.cookie.userid
+  if(!userid){
+    return res.send({code:1,msg:'请先登录'})
+  }
+  // 更新数据库中对应的数据
+  UserModel.findByIdAndUpdate({_id: userid}, req.body, function (err, user) {// user是数据库中原来的数据
+    const {_id, username, type} = user
+    // node端 ...不可用
+    // const data = {...req.body, _id, username, type}
+    // 合并用户信息
+    const data = Object.assign(req.body, {_id, username, type})
+    // assign(obj1, obj2, obj3,...) // 将多个指定的对象进行合并, 返回一个合并后的对象
+    res.send({code: 0, data})
+  })
+})
+
+//搭建整体界面
+//根据cookie获取对应的user
+router.get('/user',function (req,res) {
+  const userid = req.cookies.userid
+  if(!userid){
+    return res.send({code:1,msg:'请先登录'})
+  }
+  UserModel.findOne({_id:userid},filter,function (err,user) {
+    return res.send({code: 0,data: user})
+  })
+})
+
 module.exports = router;
